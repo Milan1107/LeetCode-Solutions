8f462ea6ec9d3e6bfa27213c6efde4db4f6a6b38
Milan Gajera <milan.gajera1107@gmail.com>
2025-05-15 14:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (44.81%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -9,28 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int maxx=0;
+//     int curr=0;
+//     void calHeight(TreeNode* root)
+//     {
+//         if(root == nullptr)
+//         {   
+//             return;
+//         }
+//         curr++;
+//         if(curr>maxx)
+//         {
+//             maxx = curr;
+//         }
+//         calHeight(root->left);
+//         calHeight(root->right);
+//         curr--;
+
+//     }
+//     int maxDepth(TreeNode* root) {
+//         calHeight(root);
+//         return maxx;
+//     }
+// };
+
 class Solution {
 public:
-    int maxx=0;
-    int curr=0;
-    void calHeight(TreeNode* root)
-    {
-        if(root == nullptr)
-        {   
-            return;
-        }
-        curr++;
-        if(curr>maxx)
-        {
-            maxx = curr;
-        }
-        calHeight(root->left);
-        calHeight(root->right);
-        curr--;
-
-    }
-    int maxDepth(TreeNode* root) {
-        calHeight(root);
-        return maxx;
+   int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
     }
 };
