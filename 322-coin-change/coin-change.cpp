7326caa28658e8c8c9e1b8dac362dff4842d1255
Milan Gajera <milan.gajera1107@gmail.com>
2025-08-04 09:47:18 +0530

    Time: 20 ms (78.21%) | Memory: 17.9 MB (75.20%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,18 +1,4 @@
 class Solution {
-    // int solve(vector<int>&coins, int n)
-    // {
-    //     if(n==0)return 0;
-    //     if(n<0)return INT_MAX;
-    //     int mini = INT_MAX;
-    //     for(int i = 0;i<coins.size();i++)
-    //     {
-    //         int ans = solve(coins,n-coins[i]);
-    //         if(ans!=INT_MAX){
-    //             mini = min(mini,1+ans);
-    //         }
-    //     }
-    //     return mini;
-    // }
     int solveTab(vector<int>&nums,int n)
     {
         vector<int>dp(n+1,INT_MAX);
