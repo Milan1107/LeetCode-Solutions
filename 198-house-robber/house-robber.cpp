45de1f38cbf04bedd5ca64db4ad5fb7c38d86510
Milan Gajera <milan.gajera1107@gmail.com>
2025-08-05 20:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.56%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,43 @@
 class Solution {
-    int solve(vector<int>&nums,int n,vector<int>&dp)
+    // int solve(vector<int>&nums,int n,vector<int>&dp)
+    // {
+    //     if(n<0)return 0;
+    //     if(n==0)return nums[0];
+
+    //     if(dp[n]!=-1)return dp[n];
+
+    //     int inc = solve(nums,n-2,dp)+nums[n];
+    //     int exc = solve(nums,n-1,dp)+0;
+
+    //     dp[n] = max(inc,exc);
+    //     return dp[n];
+    // }
+    int solve(vector<int>&nums)
     {
-        if(n<0)return 0;
-        if(n==0)return nums[0];
+        int n = nums.size();
+        if(n==0)return 0;
+        if(n==1)return nums[0];
 
-        if(dp[n]!=-1)return dp[n];
+        vector<int>dp(n,0);
 
-        int inc = solve(nums,n-2,dp)+nums[n];
-        int exc = solve(nums,n-1,dp)+0;
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        int inc,exc;
 
-        dp[n] = max(inc,exc);
-        return dp[n];
+        for(int i=2;i<n;i++)
+        {
+            inc = dp[i-2]+nums[i];
+            exc = dp[i-1];
+            dp[i] = max(inc,exc);
+        }
+        return dp[n-1];
     }
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size()-1;
-        vector<int>dp(n+1,-1);
-        int ans = solve(nums,n,dp);
+        // int n = nums.size()-1;
+        // vector<int>dp(n+1,-1);
+        // int ans = solve(nums,n,dp);
+        int ans = solve(nums);
         return ans;
     }
 };
