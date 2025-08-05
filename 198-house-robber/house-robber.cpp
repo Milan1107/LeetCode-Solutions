dc7fd73902d229c731daeb15e10fd813041d0a2a
Milan Gajera <milan.gajera1107@gmail.com>
2025-08-05 21:05:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (80.27%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,4 +1,7 @@
 class Solution {
+
+    // < -----     Memoization Approach    ----- >
+
     // int solve(vector<int>&nums,int n,vector<int>&dp)
     // {
     //     if(n<0)return 0;
@@ -12,31 +15,61 @@ class Solution {
     //     dp[n] = max(inc,exc);
     //     return dp[n];
     // }
+
+    // < -----     Tabulation Method ----- >
+    // int solveTab(vector<int>&nums)
+    // {
+    //     int n = nums.size();
+    //     if(n==0)return 0;
+    //     if(n==1)return nums[0];
+
+    //     vector<int>dp(n,0);
+
+    //     dp[0]=nums[0];
+    //     dp[1]=max(nums[0],nums[1]);
+    //     int inc,exc;
+
+    //     for(int i=2;i<n;i++)
+    //     {
+    //         inc = dp[i-2]+nums[i];
+    //         exc = dp[i-1];
+    //         dp[i] = max(inc,exc);
+    //     }
+    //     return dp[n-1];
+    // }
+
     int solve(vector<int>&nums)
     {
         int n = nums.size();
         if(n==0)return 0;
         if(n==1)return nums[0];
 
-        vector<int>dp(n,0);
+        // vector<int>dp(n,0);
 
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        int inc,exc;
+        int prev, preprev;
+        preprev=nums[0];
+        prev=max(nums[0],nums[1]);
+        int inc,exc,dp;
+        dp = prev;
 
         for(int i=2;i<n;i++)
         {
-            inc = dp[i-2]+nums[i];
-            exc = dp[i-1];
-            dp[i] = max(inc,exc);
+            inc = preprev+nums[i];
+            exc = prev;
+            dp = max(inc,exc);
+            preprev = prev;
+            prev = dp;
         }
-        return dp[n-1];
+        return dp;
     }
 public:
     int rob(vector<int>& nums) {
         // int n = nums.size()-1;
         // vector<int>dp(n+1,-1);
         // int ans = solve(nums,n,dp);
+
+        // int ans = solveTab(nums);
+
         int ans = solve(nums);
         return ans;
     }
